6409475e7dd7ad515da1ca7b67da63f4d6c6457c
NAVANEETHPL <85995846+NAVANEETHPL@users.noreply.github.com>
2021-08-04 15:08:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,8 +50,6 @@ echo
 
 read -p $'\e[34m[\e[31m*\e[34m]\e[32m Enter your choice : ' userps1
  
-tput civis
-
 echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
         command_not_found_handle() {
                 /data/data/com.termux/files/usr/libexec/termux/command-not-found \"\$1\"
